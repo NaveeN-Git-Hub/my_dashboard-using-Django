00b8ac9838e232f0b72c8e42115022c78a830846
NaveeN <youtoonaveen@gmail.com>
2020-11-21 10:07:06 +0530

    added subprocess

--- my_dashboard_project/settings.py
+++ my_dashboard_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import sys
+import subprocess
 import django_heroku
 import dj_database_url
 from decouple import config  # to set the secrete values in .env file
